4033f77474a9623b0ebf5ee5e2db84e6fd220818
Talha <talha.asif@ymail.com>
2022-03-04 01:27:53 +0500

    Initial Commit

---
+++ Test Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Swift UI
+//
+//  Created by Mnet on 04/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
